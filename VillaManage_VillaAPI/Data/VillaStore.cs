3f008518b68acb01a3b2efb0d068ba7548a3e67b
Xuho <you@example.com>
2025-02-14 09:35:35 +0700

    push first crud api

---
+++ VillaManage_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using VillaManage_VillaAPI.Model.DTO;
+
+namespace VillaManage_VillaAPI.Data
+{
+    public class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO { Id = 1, Name = "Pool View",Occupancy =4, Sqft =100 },
+                new VillaDTO { Id = 2, Name = "Beach View" ,Occupancy =3, Sqft =50}
+            };
+    }
+}
